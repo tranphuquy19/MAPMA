f5e39d7f26a8b9ad3c7bd1d11abc30037e1f83be
tranphuquy19 <tranphuquy19@gmail.com>
2019-08-15 01:16:23 +0700

    fix url

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.io = io;
 
 setInterval(()=>{
   var data = deviceInfoController.node_info;
+  data.totalclient = io.engine.clientsCount;
   io.sockets.emit('server-response', data);
 }, 3000);
 
