c0ae7075096c04a57c601ead65c68f2ef8773325
tranphuquy19 <tranphuquy19@gmail.com>
2019-08-18 18:03:40 +0700

    show info

--- controllers/deviceController.js
+++ controllers/deviceController.js
@@ -1,15 +1,19 @@
 const os = require('os');
+const packageInfo = require('../package.json');
 
 exports.node_homepage = (req, res, next) => {
     res.render('index', { title: 'MAPMA - Make Android phone more Awesome!' });
 };
 exports.node_info = {
     arch: os.arch(),
-    cpus: os.cpus(),
-    freemen: os.freemem(),
+    freemem: os.freemem(),
     totalmem: os.totalmem(),
     homedir: os.homedir(),
     hostname: os.hostname(),
     platform: os.platform(),
-    ostype: os.type()
+    ostype: os.type(),
+    nodeversion: process.version,
+    expressVer: packageInfo.dependencies['express'],
+    ejsVer: packageInfo.dependencies['ejs'],
+    socketioVer: packageInfo.dependencies['socket.io']
 }
